7f750ac353cfab40581c32ba98db0e18847b1be2
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-08-23 21:27:01 -0300

    somethings

--- frontend/frontend/src/utils/jotai.js
+++ frontend/frontend/src/utils/jotai.js
@@ -3,5 +3,6 @@ import { atomWithStorage } from 'jotai/utils';
 const tokenAtom = atomWithStorage('token', '');
 const userAtom = atomWithStorage('user', '');
 const productsAtom = atomWithStorage('products', []);
+const listsAtom = atomWithStorage('lists', []);
 
-export { tokenAtom, userAtom, productsAtom };
+export { tokenAtom, userAtom, productsAtom, listsAtom };
